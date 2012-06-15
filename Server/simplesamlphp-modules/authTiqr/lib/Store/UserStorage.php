5a53068deeb44f06a056c2721527b2b6cd7dc784
Andjelko Horvat <andjelko.horvat.comel@gmail.com>
2012-06-15 15:48:26 +0200

    Server: created Tiqr_UserStorage_GenericStore from Tiqr_UserStorage_File; modified file store; created user store for SSP.

---
+++ Server/simplesamlphp-modules/authTiqr/lib/Store/UserStorage.php
@@ -0,0 +1,50 @@
+<?php
+
+class sspmod_authTiqr_Store_UserStorage extends Tiqr_UserStorage_GenericStore {
+
+	private $store;
+
+	private $dataType = 'authTiqr:user';
+
+
+	public function __construct($config) {
+		parent::__construct($config);
+
+		$this->store = SimpleSAML_Store::getInstance();
+
+		if ($this->store === FALSE) {
+			throw new Exception('Datastore not configured.');
+		}
+
+		if (isset($config['data.type']) && is_string($config['data.type'])) {
+			$this->dataType = $config['data.type'];
+		}
+	}
+
+
+	protected function _loadUser($userId, $failIfNotFound = TRUE) {
+		$data = $this->store->get($this->dataType, $userId);
+
+		if ($data === NULL) {
+			if ($failIfNotFound) {
+				throw new Exception('Error loading data for user: ' . var_export($userId, TRUE));
+			} else {
+				return false;
+			}
+		} else {
+			return $data;
+		}
+	}
+
+
+	protected function _saveUser($userId, $data) {
+		$this->store->set($this->dataType, $userId, $data, NULL);
+
+		return true;
+	}
+
+
+	protected function _deleteUser($userId) {
+		$this->store->delete($this->dataType, $userId);
+	}
+}
