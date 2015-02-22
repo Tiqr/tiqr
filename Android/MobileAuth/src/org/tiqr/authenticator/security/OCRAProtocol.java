874582b56811e1b2eca250f9b1f2b5598462e0df
Ivo Jansch <ivo@egeniq.com>
2015-02-22 10:41:47 +0100

    Removed all sources that have now been moved to their own repositories.

--- Android/MobileAuth/src/org/tiqr/authenticator/security/OCRAProtocol.java
+++
@@ -1,7 +0,0 @@
-package org.tiqr.authenticator.security;
-
-import org.tiqr.authenticator.exceptions.InvalidChallengeException;
-
-public interface OCRAProtocol {
-    public abstract String generateOCRA(String ocraSuite, byte[] key, String challengeQuestion, String sessionKey) throws InvalidChallengeException;
-}
--- Server/simplesamlphp-demo/www/register.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Dummy implementation that uses the deviceToken as the notificationToken
-$newToken = $_POST['deviceToken'];
-
-echo $newToken;
